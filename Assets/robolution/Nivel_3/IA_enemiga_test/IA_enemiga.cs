9d526bf25738c06723eec1bc15a7071638ff8a7f
Jonathan <jothagoo@gmail.com>
2020-03-24 03:12:20 -0300

    seguimiento

---
+++ Assets/robolution/Nivel_3/IA_enemiga_test/IA_enemiga.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IA_enemiga : MonoBehaviour
+{
+    public GameObject Target;
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    public float distance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(Target.transform.position, transform.position) < distance)
+        {
+            agent.SetDestination(Target.transform.position);
+            agent.speed = 3;
+        }
+        else
+        {
+            agent.speed = 0;
+        }   
+    }
+}
