a4a028e5823f68813e5ac77d0d56e6a7a4ef9a66
Sebastian <sebastianrugby1996@gmail.com>
2020-03-26 11:21:04 -0300

    enemigo test 1

---
+++ Assets/robolution/Nivel_4/enemigos/enemigo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemigo : MonoBehaviour
+{
+    public Transform jugador;
+    UnityEngine.AI.NavMeshAgent Enemigo;
+    private bool dentro = false;
+
+    void Start()
+    {
+        Enemigo = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player"){
+            dentro = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.tag == "Player"){
+            dentro = false;
+        }
+    }
+
+
+    void Update()
+    {
+        if(!dentro){
+            Enemigo.destination = jugador.position;
+        }
+        if(dentro){
+            Enemigo.destination = this.transform.position;
+        }
+    }
+}
